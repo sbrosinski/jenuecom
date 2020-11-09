52e280f8173c646a65b5758b29b7d3cfb9cce7e4
Stephan <s@brosinski.com>
2020-11-09 15:17:11 +0100

    filter

--- .eleventy.js
+++ .eleventy.js
@@ -4,7 +4,7 @@ module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy('static');
     eleventyConfig.addPlugin(syntaxHighlight);
     eleventyConfig.addCollection("poemsDesc", (collection) =>
-      collection.getFilteredByGlob("content/poems/*.md").sort((a, b) => {
+      collection.getFilteredByGlob("content/poems/*.md").filter(item => !['index'].includes(item.tags)).sort((a, b) => {
         if (a.data.title > b.data.title) return 1;
         else if (a.data.title < b.data.title) return -1;
         else return 0;
